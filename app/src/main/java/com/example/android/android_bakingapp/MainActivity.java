6fd0422be33d89a3e9100cfe3ce4e6b083429f2f
Vicuko <victorc.fernandez@gmail.com>
2019-02-14 19:39:30 +0100

    Create Database class

--- app/src/main/java/com/example/android/android_bakingapp/MainActivity.java
+++ app/src/main/java/com/example/android/android_bakingapp/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.android.android_bakingapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.List;
+
+import androidx.appcompat.app.AppCompatActivity;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.github.com/")
+                .build();
+
+        GitHubService service = retrofit.create(GitHubService.class);
+        Call<List<String>> repos = service.listRepos("octocat");
+        Timber.d("The information obtained from Github is: %s", repos);
     }
+}
+
+interface GitHubService {
+    @GET("users/{user}/repos")
+    Call<List<String>> listRepos(@Path("user") String user);
 }
---
+++ app/src/main/java/com/example/android/android_bakingapp/data/AppDatabase.java
@@ -0,0 +1,29 @@
+package com.example.android.android_bakingapp.data;
+
+import androidx.annotation.NonNull;
+import androidx.room.DatabaseConfiguration;
+import androidx.room.InvalidationTracker;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteOpenHelper;
+
+/**
+ * Created by Vicuko on 14/2/19.
+ */
+public class AppDatabase extends RoomDatabase {
+    @NonNull
+    @Override
+    protected SupportSQLiteOpenHelper createOpenHelper(DatabaseConfiguration config) {
+        return null;
+    }
+
+    @NonNull
+    @Override
+    protected InvalidationTracker createInvalidationTracker() {
+        return null;
+    }
+
+    @Override
+    public void clearAllTables() {
+
+    }
+}
