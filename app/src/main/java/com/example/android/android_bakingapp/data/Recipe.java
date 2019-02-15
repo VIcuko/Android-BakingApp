665a122d8d423d13345491e9ffb0c7723c78a949
Vicuko <victorc.fernandez@gmail.com>
2019-02-15 19:45:21 +0100

    Create Recipe class for Recipe entity and inform it with required variables

---
+++ app/src/main/java/com/example/android/android_bakingapp/data/Recipe.java
@@ -0,0 +1,61 @@
+package com.example.android.android_bakingapp.data;
+
+import android.os.Parcel;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+/**
+ * Created by Vicuko on 15/2/19.
+ */
+@Entity(tableName = "recipe")
+public class Recipe {
+
+    @NonNull
+    @PrimaryKey
+    @SerializedName("id")
+    @Expose
+    private long id;
+
+    @SerializedName("name")
+    @Expose
+    private String name;g
+
+    @Ignore
+    @SerializedName("ingredients")
+    @Expose
+    private List<Ingredient> ingredients;
+
+    @Ignore
+    @SerializedName("steps")
+    @Expose
+    private List<Step> steps;
+
+    @SerializedName("servings")
+    @Expose
+    private long servings;
+
+    @SerializedName("image")
+    @Expose
+    private String image;
+
+    public Recipe() {
+    }
+
+    @Ignore
+    protected Recipe(Parcel in) {
+        id = in.readLong();
+        name = in.readString();
+        ingredients = in.createTypedArrayList(Ingredient.CREATOR);
+        steps = in.createTypedArrayList(Step.CREATOR);
+        servings = in.readLong();
+        image = in.readString();
+    }
+
+
+}
--- app/src/main/java/com/example/android/android_bakingapp/data/RecipesDao.java
+++ app/src/main/java/com/example/android/android_bakingapp/data/RecipesDao.java
@@ -1,7 +1,27 @@
 package com.example.android.android_bakingapp.data;
 
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Update;
+
 /**
  * Created by Vicuko on 14/2/19.
  */
 public interface RecipesDao {
+
+    @Query("SELECT * FROM movies")
+    LiveData<List<MovieEntry>> loadAllTasks();
+
+    @Query("SELECT * FROM movies WHERE id = :id")
+    LiveData<MovieEntry> loadMovieById(int id);
+
+    @Insert
+    void insertMovie(MovieEntry movieEntry);
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    void updateMovie(MovieEntry movieEntry);
+
+    @Delete
+    void deleteMovie(MovieEntry movieEntry);
 }
